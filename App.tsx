3a40124e0c4bd74b406be894c8773d6619380920
Felipe Simmi <simmifelipe1992@gmail.com>
2021-06-15 21:54:02 -0300

    Implementada a configuraçao inicial da autenticação com o Firebase

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/routes/tabs';
 import { StatusBar } from 'react-native';
 import { COLORS } from './src/constants';
+import SignIn from './src/screens/SignIn';
 
 const Stack = createStackNavigator();
 
@@ -21,10 +22,11 @@ const App = () => {
         screenOptions={{
           headerShown: false,
         }}
-        initialRouteName={'Home'}>
+        initialRouteName={'SignIn'}>
         <Stack.Screen name="Home" component={Tabs} />
 
         <Stack.Screen name="MovieDetail" component={MovieDetail} />
+        <Stack.Screen name="SignIn" component={SignIn} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
-import { View } from 'react-native';
+import { useEffect } from 'react';
+import { Alert, View } from 'react-native';
 
-// import { Container } from './styles';
+import auth from '@react-native-firebase/auth';
 
 const SignIn: React.FC = () => {
+  useEffect(() => {
+    async function createAndSignIn() {
+      try {
+        const response = await auth().createUserWithEmailAndPassword(
+          'felipe.simmi@gmail.com',
+          '123456',
+        );
+        console.log(response.user);
+        Alert.alert('Sucesso', 'Usuário criado e logado com sucesso!');
+      } catch (err) {
+        if (err.code === 'auth/email-already-in-use') {
+          Alert.alert(
+            'Autenticação',
+            'E-mail já está sendo utilizado por outra conta!',
+          );
+        }
+        if (err.code === 'auth/invalid-email') {
+          Alert.alert('Autenticação', 'E-mail informado é inválido!');
+        }
+      }
+    }
+
+    createAndSignIn();
+  }, []);
+
   return <View />;
 };
 
